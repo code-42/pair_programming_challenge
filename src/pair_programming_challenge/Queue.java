72b8b65be2aa5a4cd75914cb332f60187d41f069
Edward Dupre <edwd42@gmail.com>
2018-07-05 20:24:56 -0400

    add class Queue

---
+++ src/pair_programming_challenge/Queue.java
@@ -0,0 +1,21 @@
+package pair_programming_challenge;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Queue {
+	
+	// request sent to queue
+	private String R;
+	
+	Queue<Integer> q = new LinkedLIst<>();
+	
+	public void enQueue() {
+		
+	}
+	
+	public void deQueue() {
+		
+	}
+
+}
